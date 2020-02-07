4c2b66040018b6d64ab3b6c55fce655b501c9688
Anton <qwer022022000@mail.ru>
2020-02-07 22:04:00 +0300

    Refactoring check_events in game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -1,5 +1,25 @@
 import pygame
 
+def check_keydown_events(event, ship):
+	"""Check keydown events"""
+	#Move right
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = True
+	#Move left
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+	"""Check keyup events"""
+	#Move right
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+	#Move left
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
+
+
 def check_events(ship):
 	"""Check keyboard and mouse events"""
 	for event in pygame.event.get():
@@ -8,21 +28,11 @@ def check_events(ship):
 
 		#Start moving
 		elif event.type == pygame.KEYDOWN:
-			#Move right
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = True
-			#Move left
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = True
+			check_keydown_events(event, ship)
 
 		#Stop moving
 		elif event.type == pygame.KEYUP:
-			#Move right
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = False
-			#Move left
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = False
+			check_keyup_events(event, ship)
 
 
 def update_screen(settings, screen, ship):
